ff148b986c078861e6946995bda2b9a993959824
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2022-12-02 14:23:26 +0000

    Lesson 23: Tasks web site with Handlebars as view engine, using layout and partial views.

--- lesson23/tasks-server.mjs
+++ lesson23/tasks-server.mjs
@@ -10,9 +10,15 @@ import tasksServicesInit from './services/tasks-services.mjs'
 import tasksApiInit from './web/api/tasks-http-api.mjs'
 import tasksSiteInit from './web/site/tasks-http-site.mjs'
 
+
+
+
 import swaggerUi from 'swagger-ui-express'
 import yaml from 'yamljs'
 import cors from 'cors'
+import hbs from 'hbs'
+import url from 'url'
+import path from 'path'
 
 const tasksServices = tasksServicesInit(tasksData, usersData)
 const tasksApi = tasksApiInit(tasksServices)
@@ -28,14 +34,25 @@ app.use(cors())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(express.json())
 
+// view engine setup
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'web', 'site', 'views'));
+//hbs.registerPartials(__dirname + '/views/partials');
+
+
 app.get('/home', tasksSite.getHome)
+app.get('/tasks/newTask', tasksSite.getNewTask)
+app.get('/tasks/:id', tasksSite.getTask)
+app.get('/tasks', tasksSite.getTasks)
 app.get('/site.css', tasksSite.getCss)
 
-app.get('/tasks', tasksApi.getTasks)
-app.get('/tasks/:id', tasksApi.getTask)
-app.delete('/tasks/:id', tasksApi.deleteTask)
-app.post('/tasks', tasksApi.createTask)
-app.put('/tasks/:id', tasksApi.updateTask)
+app.get('/api/tasks', tasksApi.getTasks)
+app.get('/api/tasks/:id', tasksApi.getTask)
+app.delete('/api/tasks/:id', tasksApi.deleteTask)
+app.post('/api/tasks', tasksApi.createTask)
+app.put('/api/tasks/:id', tasksApi.updateTask)
 
 app.listen(PORT, () => console.log(`Server listening in http://localhost:${PORT}`))
 
--- lesson23/web/site/tasks-http-site.mjs
+++ lesson23/web/site/tasks-http-site.mjs
@@ -5,6 +5,7 @@
 //  - Generate the response in HTML format
 
 import url from 'url'
+import toHttpResponse from '../api/response-errors.mjs'
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
@@ -14,19 +15,59 @@ export default function (tasksServices) {
     return {
         getHome: getHome,
         getCss: getCss,
+        getCss: getCss,
+        getTask: handleRequest(getTask),
+        getTasks: handleRequest(getTasks),
+        getNewTask: getNewTask,
     
     }
 
     async function getHome(req, rsp) {
-      const htmlFileLocation = __dirname + 'public/index.html'
-        console.log(htmlFileLocation)
-        rsp.sendFile(htmlFileLocation)
+      sendFile('index.html', rsp)
     }
 
     async function getCss(req, rsp) {
-      const htmlFileLocation = __dirname + 'public/site.css'
-        rsp.sendFile(htmlFileLocation)
+      sendFile('site.css', rsp)
+    }
+
+    async function getTasks(req, rsp) {
+      const tasks = await tasksServices.getTasks(req.token, req.query.q, req.query.skip, req.query.limit)
+      return { name: 'tasks', data: { title: 'All tasks', tasks: tasks} }  
+    }
+
+    async function getTask(req, rsp) {
+      const taskId = req.params.id
+      const task = await tasksServices.getTask(req.token, taskId)
+      return { name: 'task', data: task } 
     }
     
+    async function getNewTask(req, rsp) {
+      rsp.render('newTask') 
+    }
+
+    function sendFile(fileName, rsp) {
+      const fileLocation = __dirname + 'public/' + fileName
+      rsp.sendFile(fileLocation)
+    }
+
+
+    function handleRequest(handler) {
+      // TODO: While we do not have authentication in site interface, 
+      // let's hardcode a token for one user 
+      const HAMMER_TOKEN = 'ef604e80-a351-4d13-b78f-c888f3e63b61'
+      return async function(req, rsp) {
+          req.token = HAMMER_TOKEN
+          try {
+              let view = await handler(req, rsp)
+              rsp.render(view.name , view.data)
+          } catch(e) {
+              // TODO: Hammer time again. We are in an HTML response format
+              // returning errors in Json format
+              const response = toHttpResponse(e)
+              rsp.status(response.status).json(response.body)
+              console.log(e)
+          }
+      }
+  }
 }
 
